cb62602c7ab7413dcba24a4ca4f4d011bcefe099
Tagomori0211 <tagomoriyuukichi@gmail.com>
2025-11-27 06:21:02 +0000

    GCW->code-server env change

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -103,3 +103,58 @@ resource "proxmox_vm_qemu" "mcbe_server_vm" {
   ${var.ssh_public_key}
   EOF
 }
+# main.tf の末尾に追記
+
+# ---------------------------------------------------------
+# 3. Dev Workstation (新しい開発拠点 / code-server)
+# ---------------------------------------------------------
+resource "proxmox_vm_qemu" "dev_workstation" {
+  name        = "dev-workstation"
+  target_node = "pve"
+  clone       = "ubuntu-2204-cloudinit-template"
+  
+  full_clone = true # 開発機なので独立させる
+
+  # 自宅サーバーの富豪スペックを活かす！
+  cores   = 8       # 8コア！
+  sockets = 1
+  memory  = 32768   # 32GB RAM！ (GCWより強い！)
+  scsihw  = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  # 画面設定
+  vga {
+    type = "std"
+  }
+
+  # Cloud-Initドライブ
+  disk {
+    slot    = "ide2"
+    type    = "cloudinit"
+    storage = "local-lvm"
+  }
+
+  # メインディスク (開発用なので少し大きめに)
+  disk {
+    slot     = "scsi0"
+    size     = "100G"
+    type     = "disk"
+    storage  = "local-lvm"
+    iothread = true
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  os_type = "cloud-init"
+  # 固定IP: .22 を割り当て
+  ipconfig0 = "ip=192.168.0.22/24,gw=192.168.0.1"
+  ciuser  = "tagomori"
+  cipassword = "password123"
+  sshkeys = <<EOF
+  ${var.ssh_public_key}
+  EOF
+}
